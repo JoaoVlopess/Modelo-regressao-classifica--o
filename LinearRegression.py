5febd4e2bf3ce0c3f4a740cd736d34b1961f3228
Isaac Newton <isaacnewton.montinegro@gmail.com>
2026-03-20 14:22:45 -0300

    commit isaac maneiro

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,20 +1,84 @@
 import numpy as np
 
 # REGRESSÃO parte 3
-class MeanModel:
-    def __init__(self): 
-        self.mean_value = None
 
-    def fit(self,X_train, y_train):
+
+class MeanModel:
+    def __init__(self, y_train):
+        self.y_train = y_train
+
+    def fit(self):
         """
         Essa função é responsável por calcular a média de y e guardar no objeto
         """
-        self.mean_value = np.sum(y_train)/np.shape(y_train)[0]
-        return self
-    
-    def predict (self, X_train):
+        self.beta_0 = np.mean(self.y_train)
+
+    def predict(self, X_test):
         """
         Essa função é responsável por fazer a predição do modelo
         """
-        return np.full(shape=(len(X_train) ,1), fill_value=self.mean_value)
+        return np.full(shape=(len(X_test), 1), fill_value=self.beta_0)
 
+
+class LinearRegression:
+    def __init__(self, X_train, y_train, intercepto=True, lambda_=0.0):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.intercepto = intercepto
+        self.lambda_ = lambda_
+        if self.y_train.ndim == 1:
+            self.y_train = self.y_train.reshape(-1, 1)
+        self.N, self.p = X_train.shape
+        if self.intercepto:
+            self.X_train = np.hstack((
+                np.ones((self.N, 1)),
+                self.X_train
+            ))
+
+        self.beta_hat = None
+
+    def fit(self):
+        n_features = self.X_train.shape[1]
+        I = np.eye(n_features)
+        if self.intercepto:
+            I[0, 0] = 0
+        self.beta_hat = np.linalg.inv(
+            self.X_train.T @ self.X_train + self.lambda_ * I) @ self.X_train.T @ self. y_train
+        bp = 1  # vi no código do cirillo e não entendi muito bem
+
+    def predict(self, X_test):
+        X_test = np.array(X_test, dtype=float)
+        if X_test.ndim == 1:
+            X_test = X_test.reshape(-1, 1)
+        N = X_test.shape[0]
+        if self.intercepto:
+            if len(X_test.shape) > 2:
+                X_test = np.concatenate((np.ones((N, N, 1)), X_test), axis=2)
+            else:
+                X_test = np.hstack((np.ones((N, 1)), X_test))
+        return X_test@self.beta_hat
+
+
+class TrainTest:
+    def train_test_split(self, X, y, train_size=0.8):
+        N = X.shape[0]
+        indices = np.random.permutation(N)
+
+        n_train = int(N * train_size)
+        train_indices = indices[:n_train]
+        test_indices = indices[n_train:]
+
+        X_train = X[train_indices]
+        y_train = y[train_indices]
+        X_test = X[test_indices]
+        y_test = y[test_indices]
+
+        return X_train, y_train, X_test, y_test
+
+    def mean_squared_error(self, y_true, y_pred):
+        return np.mean((y_true - y_pred) ** 2)
+
+    def r2_score(self, y_true, y_pred):
+        ss_res = np.sum((y_true - y_pred) ** 2)
+        ss_tot = np.sum((y_true - np.mean(y_true)) ** 2)
+        return 1 - (ss_res / ss_tot)
--- main.py
+++ main.py
@@ -1,22 +1,139 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from LinearRegression import MeanModel
+from LinearRegression import LinearRegression
+from LinearRegression import TrainTest
+
+
 
 # REGRESSÃO parte 1/2
-data = np.loadtxt('data/aerogerador (1).dat')
+data = np.loadtxt('C:\\Users\\Samsung\\OneDrive\\Área de Trabalho\\Programação\\IA\\Trabalho\\Modelo-regressao-classifica--o\\data\\aerogerador (1).dat')
 X = data[:,:-1]
 y = data[:,-1:]
+x_line = np.linspace(X.min(), X.max(), 100).reshape(-1, 1)
 
-plt.scatter(X, y, edgecolors = '0')
 
-#REGRESSÃO parte 3
+#REGRESSÃO parte 3/4
+# Criando o modelo de médias
 
-modelo_media = MeanModel()
-modelo_media.fit(X, y)
+fig1 = plt.figure(1)
+modelo_media = MeanModel(y)
+modelo_media.fit()
 
-y_hat = modelo_media.predict([[14], [10], [3], [5]])
+media_y = modelo_media.beta_0
+
+x_reta = np.linspace(0, 14, 100).reshape(-1, 1)
+
+X_teste = [[9], [10], [1], [5]]
+y_hat = modelo_media.predict(X_teste)
 plt.scatter(X, y, color='blue', label='Dados Reais', edgecolors='0')
-plt.plot([[14], [10], [3], [5]], y_hat, color='red', linewidth=3, label='Modelo de Médias')
+plt.axhline(y=modelo_media.beta_0, color='magenta', linestyle='--', label='Média')
+plt.plot(X_teste, y_hat, color='cyan', linewidth=3, label='Modelo de Médias')
 plt.legend()
 plt.title("Visualização do Modelo de Médias")
+
+# MQO TRADICIONAL
+
+fig2 = plt.figure(2)
+modelo_mqo = LinearRegression(X, y, intercepto=False, lambda_=0.0) 
+modelo_mqo.fit()
+
+y_hat = modelo_mqo.predict(x_line)
+plt.scatter(X, y, color='blue', label='Dados Reais', edgecolors='0')
+plt.plot(x_line, y_hat, color='red', linewidth=3, label='MQO Tradicional')
+plt.legend()
+plt.title("Visualização do Modelo MQO Tradicional")
+
+
+# MQO REGULARIZADO (tikhonov)
+fig3 = plt.figure(3)
+
+modelo_tikhonov1 = LinearRegression(X, y, intercepto=False, lambda_=0.25)
+modelo_tikhonov2 = LinearRegression(X, y, intercepto=False, lambda_=0.50)
+modelo_tikhonov3 = LinearRegression(X, y, intercepto=False, lambda_=0.75)
+modelo_tikhonov4 = LinearRegression(X, y, intercepto=False, lambda_= 1.0)
+
+modelo_tikhonov1.fit()
+modelo_tikhonov2.fit()
+modelo_tikhonov3.fit()
+modelo_tikhonov4.fit()
+
+y_hat1 = modelo_tikhonov1.predict(x_line)
+y_hat2 = modelo_tikhonov2.predict(x_line)
+y_hat3 = modelo_tikhonov3.predict(x_line)
+y_hat4 = modelo_tikhonov4.predict(x_line)
+
+plt.scatter(X, y, color='blue', label='Dados Reais', edgecolors='0')
+plt.plot(x_line, y_hat1, color='red', linewidth=3, label='MQO Regularizado (Tikhonov)')
+plt.plot(x_line, y_hat2, color='blue', linewidth=3, label='MQO Regularizado (Tikhonov)')
+plt.plot(x_line, y_hat3, color='green', linewidth=3, label='MQO Regularizado (Tikhonov)')
+plt.plot(x_line, y_hat4, color='orange', linewidth=3, label='MQO Regularizado (Tikhonov)')
+plt.legend()
+plt.title("Visualização do Modelo MQO Regularizado (Tikhonov)")
+
+
 plt.show()
+
+# REGRESSÃO parte 5
+TT = TrainTest()
+R = 500
+
+resultados = {
+    'MeanModel': {'MSE': [], 'R2': []},
+    'MQO': {'MSE': [], 'R2': []},
+    'Tikhonov_0.25': {'MSE': [], 'R2': []},
+    'Tikhonov_0.50': {'MSE': [], 'R2': []},
+    'Tikhonov_0.75': {'MSE': [], 'R2': []},
+    'Tikhonov_1.00': {'MSE': [], 'R2': []}
+}
+
+for _ in range(R):
+    X_train, X_test, y_train, y_test = TT.train_test_split(X, y)
+
+    # 1. MeanModel
+    modelo_mean = MeanModel(y_train)
+    modelo_mean.fit()
+    y_pred = modelo_mean.predict(X_test)
+
+    resultados['MeanModel']['MSE'].append(TT.mean_squared_error(y_test, y_pred))
+    resultados['MeanModel']['R2'].append(TT.r2_score(y_test, y_pred))
+
+    # 2. MQO tradicional
+    modelo_mqo = LinearRegression(X_train, y_train, intercepto=True, lambda_=0.0)
+    modelo_mqo.fit()
+    y_pred = modelo_mqo.predict(X_test)
+
+    resultados['MQO']['MSE'].append(TT.mean_squared_error(y_test, y_pred))
+    resultados['MQO']['R2'].append(TT.r2_score(y_test, y_pred))
+
+    # 3. Tikhonov lambda = 0.25
+    modelo_t1 = LinearRegression(X_train, y_train, intercepto=True, lambda_=0.25)
+    modelo_t1.fit()
+    y_pred = modelo_t1.predict(X_test)
+
+    resultados['Tikhonov_0.25']['MSE'].append(TT.mean_squared_error(y_test, y_pred))
+    resultados['Tikhonov_0.25']['R2'].append(TT.r2_score(y_test, y_pred))
+
+    # 4. Tikhonov lambda = 0.50
+    modelo_t2 = LinearRegression(X_train, y_train, intercepto=True, lambda_=0.50)
+    modelo_t2.fit()
+    y_pred = modelo_t2.predict(X_test)
+
+    resultados['Tikhonov_0.50']['MSE'].append(TT.mean_squared_error(y_test, y_pred))
+    resultados['Tikhonov_0.50']['R2'].append(TT.r2_score(y_test, y_pred))
+
+    # 5. Tikhonov lambda = 0.75
+    modelo_t3 = LinearRegression(X_train, y_train, intercepto=True, lambda_=0.75)
+    modelo_t3.fit()
+    y_pred = modelo_t3.predict(X_test)
+
+    resultados['Tikhonov_0.75']['MSE'].append(TT.mean_squared_error(y_test, y_pred))
+    resultados['Tikhonov_0.75']['R2'].append(TT.r2_score(y_test, y_pred))
+    
+     # 6. Tikhonov lambda = 1.00
+    modelo_t4 = LinearRegression(X_train, y_train, intercepto=True, lambda_=1.00)
+    modelo_t4.fit()
+    y_pred = modelo_t4.predict(X_test)
+
+    resultados['Tikhonov_1.00']['MSE'].append(TT.mean_squared_error(y_test, y_pred))
+    resultados['Tikhonov_1.00']['R2'].append(TT.r2_score(y_test, y_pred))
